dce90f0e19d5d1b76d9d029b39d7b21074d41f74
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-03-17 11:20:31 +0900

    Update Sherlock-and-Anagrams.cpp

--- Sherlock-and-Anagrams.cpp
+++ Sherlock-and-Anagrams.cpp
@@ -1,3 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'sherlockAndAnagrams' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
 int sherlockAndAnagrams(string s)
 {
     int result = 0;
