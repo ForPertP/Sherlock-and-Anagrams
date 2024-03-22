d90bc657642e4e30cfcd7be968166164cb82e3a7
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-03-22 09:29:56 +0900

    Update Sherlock-and-Anagrams.cs

--- Sherlock-and-Anagrams.cs
+++ Sherlock-and-Anagrams.cs
@@ -1,1 +1,30 @@
-CS
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'sherlockAndAnagrams' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int sherlockAndAnagrams(string s)
+    {
+
+    }
+
+}
