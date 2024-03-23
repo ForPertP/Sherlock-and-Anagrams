3e8268325dea3d0a4d634d6579045665b8e7e5c5
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-03-23 13:07:46 +0900

    Update Sherlock-and-Anagrams.cs

--- Sherlock-and-Anagrams.cs
+++ Sherlock-and-Anagrams.cs
@@ -28,3 +28,25 @@ class Result
     }
 
 }
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string s = Console.ReadLine();
+
+            int result = Result.sherlockAndAnagrams(s);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
